ee540228077b6d04cb7b1ad75c2107303bddf4f8
Krzysztof Sierszeń <krzysztof_sierszen@hotmail.com>
2022-04-16 12:17:19 +0200

    chore(verify): Since tag bump

--- src/main/groovy/org/codehaus/mojo/spotbugs/VerifyMojo.groovy
+++ src/main/groovy/org/codehaus/mojo/spotbugs/VerifyMojo.groovy
@@ -28,7 +28,7 @@ import org.apache.maven.plugins.annotations.ResolutionScope
  * Note that unlike the {@code check} goal, this goal only reads existing XML reports, <b>without</b> actually performing SpotBugs analysis.
  * To see more documentation about SpotBugs' options, please see the <a href="https://spotbugs.readthedocs.io/en/latest/" class="externalLink">SpotBugs Manual.</a>.
  *
- * @since 4.5
+ * @since 4.7
  */
 @Mojo( name = "verify", defaultPhase = LifecyclePhase.VERIFY, requiresDependencyResolution = ResolutionScope.TEST, requiresProject = true, threadSafe = true)
 class VerifyMojo extends BaseViolationCheckMojo {
