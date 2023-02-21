668e7b817599405effa395e472259949b5f7c475
Jeremy Landis <jeremylandis@hotmail.com>
2023-02-20 20:32:36 -0500

    [ci] Adjust formatting to that of original

--- src/main/groovy/org/codehaus/mojo/spotbugs/SpotBugsMojo.groovy
+++ src/main/groovy/org/codehaus/mojo/spotbugs/SpotBugsMojo.groovy
@@ -1291,11 +1291,13 @@ class SpotBugsMojo extends AbstractMavenReport implements SpotBugsPluginsTrait {
                 outputFile.createNewFile()
 
                 def writer = outputFile.newWriter(effectiveEncoding)
+
                 if (effectiveEncoding.equalsIgnoreCase("Cp1252")) {
                     writer.write "<?xml version=\"1.0\" encoding=\"windows-1252\"?>"
                 } else {
                     writer.write "<?xml version=\"1.0\" encoding=\"" + effectiveEncoding.toLowerCase(Locale.ENGLISH) + "\"?>"
                 }
+
                 writer.write "\n"
 
                 writer << xmlBuilder.bind { mkp.yield path }
