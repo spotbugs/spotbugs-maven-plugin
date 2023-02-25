a613a51776adc63c2d885f8a486889ec0781f31b
Jeremy Landis <jeremylandis@hotmail.com>
2023-02-25 17:40:01 -0500

    [tests] Add check that we are not missing classes for analysis
    Test ensures issue with aux path fix in 4.7.3.1 does not resurface.  Confirmed this test will fail with usage of 4.7.3.1 but is fixed now on latest.

---
+++ src/it/check-no-missing-classes/src/main/java/test/App.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        Assertions.assertNull(args);
+        System.out.println( "Hello World!" );
+    }
+}
