1469ead4665a906884b6b91e50cd4e0e983ec82b
Jeremy Landis <jeremylandis@hotmail.com>
2025-02-16 16:53:53 -0500

    [sonar] Add loggers instead of using system out

--- src/it-src/test/java/org/codehaus/mojo/spotbugsmavenplugin/it/App.java
+++ src/it-src/test/java/org/codehaus/mojo/spotbugsmavenplugin/it/App.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2023 the original author or authors.
+ * Copyright 2005-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,13 +15,18 @@
  */
 package org.codehaus.mojo.spotbugsmavenplugin.it;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  */
 public class App implements Cloneable {
 
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        logger.info("Hello World!");
     }
 
     public Object clone() {
--- src/it/skipEmpty/src/main/java/test/App.java
+++ src/it/skipEmpty/src/main/java/test/App.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2023 the original author or authors.
+ * Copyright 2005-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,15 +15,18 @@
  */
 package test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App
-{
+public class App {
 
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
     }
 }
