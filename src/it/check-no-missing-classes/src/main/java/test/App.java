14f4bfc7db8c064407ac8c15bb16542259ba7a25
Jeremy Landis <jeremylandis@hotmail.com>
2025-02-16 17:12:35 -0500

    [it] Rewrite test that used junit in source to use objects.assertNull() instead

--- src/it/check-no-missing-classes/src/main/java/test/App.java
+++ src/it/check-no-missing-classes/src/main/java/test/App.java
@@ -15,7 +15,8 @@
  */
 package test;
 
-import org.junit.jupiter.api.Assertions;
+import java.util.Objects;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +28,7 @@ public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        Assertions.assertNull(args);
+        Objects.assertNull(args);
         logger.info("Hello World!");
     }
 }
