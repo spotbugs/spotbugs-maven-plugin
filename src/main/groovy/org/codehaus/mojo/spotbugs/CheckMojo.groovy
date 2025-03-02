89d8332b7a3011b1f77440bc48cf334e090cac18
Jeremy Landis <jeremylandis@hotmail.com>
2025-03-01 21:23:50 -0500

    Formatting javadocs

--- src/main/groovy/org/codehaus/mojo/spotbugs/CheckMojo.groovy
+++ src/main/groovy/org/codehaus/mojo/spotbugs/CheckMojo.groovy
@@ -23,11 +23,13 @@ import org.apache.maven.plugins.annotations.ResolutionScope
 /**
  * Fail the build if there were any SpotBugs violations in the source code.
  * An XML report is put out by default in the target directory with the errors.
- * To see more documentation about SpotBugs' options, please see the <a href="https://spotbugs.readthedocs.io/en/latest/" class="externalLink">SpotBugs Manual.</a>.
+ * To see more documentation about SpotBugs' options,
+ * please see the <a href="https://spotbugs.readthedocs.io/en/latest/" class="externalLink">SpotBugs Manual.</a>.
  *
  * @since 2.0
  */
-@Mojo(name = 'check', defaultPhase = LifecyclePhase.VERIFY, requiresDependencyResolution = ResolutionScope.TEST, requiresProject = true, threadSafe = true)
+@Mojo(name = 'check', defaultPhase = LifecyclePhase.VERIFY, requiresDependencyResolution = ResolutionScope.TEST,
+         requiresProject = true, threadSafe = true)
 @Execute(goal = 'spotbugs')
 class CheckMojo extends BaseViolationCheckMojo {
     // Check Mojo to run spotbugs execution
--- src/main/groovy/org/codehaus/mojo/spotbugs/VerifyMojo.groovy
+++ src/main/groovy/org/codehaus/mojo/spotbugs/VerifyMojo.groovy
@@ -20,15 +20,18 @@ import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.ResolutionScope
 
 /**
- * Fail the build if any SpotBugs violations can be found in a preexisting {@code spotBugs.xml} file.
- * Note that unlike the {@code check} goal, this goal only reads existing XML reports, <b>without</b> actually performing SpotBugs analysis.
- * This way, it is possible to split the analysis and verification into lifecycle phases of one's choosing.
- * One use case for that is running multiple code analyzers at once and only failing the build at a later stage, so that all of them have a chance to run.
- * To see more documentation about SpotBugs' options, please see the <a href="https://spotbugs.readthedocs.io/en/latest/" class="externalLink">SpotBugs Manual.</a>.
+ * Fail the build if any SpotBugs violations can be found in a pre-existing {@code spotBugs.xml} file.
+ * <p>
+ * Note that unlike the {@code check} goal, this goal only reads existing XML reports, <b>without</b> actually
+ * performing SpotBugs analysis.  This way, it is possible to split the analysis and verification into lifecycle
+ * phases of one's choosing. One use case for that is running multiple code analyzers at once and only failing the
+ * build at a later stage, so that all of them have a chance to run. To see more documentation about SpotBugs' options,
+ * please see the <a href="https://spotbugs.readthedocs.io/en/latest/" class="externalLink">SpotBugs Manual.</a>.
  *
  * @since 4.7
  */
-@Mojo(name = 'verify', defaultPhase = LifecyclePhase.VERIFY, requiresDependencyResolution = ResolutionScope.TEST, requiresProject = true, threadSafe = true)
+@Mojo(name = 'verify', defaultPhase = LifecyclePhase.VERIFY, requiresDependencyResolution = ResolutionScope.TEST,
+        requiresProject = true, threadSafe = true)
 class VerifyMojo extends BaseViolationCheckMojo {
     // Verification Mojo to verify existing bugs rather than run spotbugs
 }
