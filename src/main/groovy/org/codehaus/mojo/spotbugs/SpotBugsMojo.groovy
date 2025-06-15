3fe6bc8f4d19e19c2971c83f30e638bd130fb5a1
Jeremy Landis <jeremylandis@hotmail.com>
2025-06-15 14:24:22 -0400

    Expand getoutputname to getoutputpath given central code deprecated even if not used yet

--- src/main/groovy/org/codehaus/mojo/spotbugs/SpotBugsMojo.groovy
+++ src/main/groovy/org/codehaus/mojo/spotbugs/SpotBugsMojo.groovy
@@ -581,10 +581,27 @@ class SpotBugsMojo extends AbstractMavenReport implements SpotBugsPluginsTrait {
      * Called by AbstractMavenReport.execute() for creating the sink.
      *
      * @return name of the generated page
-     * @see MavenReport#getOutputName()
+     * @see (@link MavenReport#getOutputName()}
+     *
+     * @deprecated Method name does not properly reflect its purpose. Implement and use
+     * {@link #getOutputPath()} instead.
      */
     @Override
+    @Deprecated
     String getOutputName() {
+        return SpotBugsInfo.PLUGIN_NAME
+    }
+
+    /**
+     * Returns report output file name, without the extension.
+     *
+     * Called by AbstractMavenReport.execute() for creating the sink.
+     *
+     * @return name of the generated page
+     * @see {@link MavenReport#getOutputPath()}
+     */
+    @Override
+    String getOutputPath() {
         return SpotBugsInfo.PLUGIN_NAME
     }
 
